8cefba23cfbcc9a63ab18966e9629425abe60cb0
Schuyler Erle <schuyler@lattice.com>
2024-02-26 20:37:38 -0800

    Working stub

--- lib/roboyoz-stack.ts
+++ lib/roboyoz-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import RoboYozService from "./roboyoz-service";
 
-export class RoboyozStack extends cdk.Stack {
+export class RoboYozStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'RoboyozQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new RoboYozService(this, "RoboYoz");
   }
 }
